3c66566a446167a0033488bd2c5bb886d4c17074
gopal-vsg <saigopalvallu7@gmail.com>
2025-02-20 11:41:22 +0530

    class functions added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const studentRoutes = require('./routes/studentRoutes');
 const batchRoutes = require('./routes/batchRoutes');
+const classRoutes = require('./routes/classRoutes');
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(cors());
 
 app.use('/api/students', studentRoutes);
 app.use('/api/batches', batchRoutes);
+app.use('/api/classes', classRoutes);
 
 const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
